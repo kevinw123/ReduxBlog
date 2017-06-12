986e5ef9bb7f18d9e83e7ad08b849e2ce75645fd
Kevin Wong <kevinwong_895@hotmail.com>
2017-06-11 22:14:04 -0400

    Showing post by id

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
+import axios from 'axios';
+
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
-
-import axios from 'axios';
+export const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=kevinw';
@@ -18,6 +19,14 @@ export function createPost(values, callback){
 
   return {
     type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
